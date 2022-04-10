65a6b9e196286295b730cd8ecc58141b921ff80a
Ivan <imogutov@me.com>
2022-04-10 19:58:05 +0300

    Работа с ViewController

--- Netology_IB_Instruments/ProfileViewController.swift
+++ Netology_IB_Instruments/ProfileViewController.swift
@@ -11,13 +11,7 @@ class ProfileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let screenWidth = UIScreen.main.bounds.width
-        let screenHeight = UIScreen.main.bounds.height
-        
-        if let myView = Bundle.main.loadNibNamed("ProfileView", owner: nil, options: nil)?.first as? ProfileView {
-            myView.frame = CGRect(x: 0, y: 0, width: screenWidth, height: screenHeight)
-            view.addSubview(myView)
-        }
+
     }
     
 }
