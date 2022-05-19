e55141f8bf8844808284102641820759e9aff02d
imogutov <imogutov@me.com>
2022-05-19 21:11:39 +0300

    Работа с файлами ProfileViewController и ProfileHeaderView

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -3,12 +3,31 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
+    private lazy var profileHeaderView: ProfileHeaderView = {
+        let profileHeaderView = ProfileHeaderView()
+        return profileHeaderView
+    }()
+    
     override func viewDidLoad() {
-        
         super.viewDidLoad()
-        
-        self.view.backgroundColor = .systemBackground
+        view.backgroundColor = .lightGray
+        view.addSubview(profileHeaderView)
         
     }
     
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        profileHeaderView.frame = CGRect(x: 0, y: view.safeAreaInsets.top, width: view.bounds.width, height: view.bounds.height - view.safeAreaInsets.top - view.safeAreaInsets.bottom)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
